a0790c603763d9917aab79399a60dd8970d16573
Hamataki Ryunosuke <168268414+hamataki@users.noreply.github.com>
2024-06-20 22:58:51 +0900

    Add files via upload
    二段階ログイン
    チェックボックスで好みのスポーツ取得

---
+++ start.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>ログイン完了</title>
+
+    <!-- フォント -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat+Subrayada" rel="stylesheet">
+</head>
+<body>
+    <h1 class="title">CheerParK</h1>
+    <form action="">
+    <h2 class="start">スタート画面</h2>
+    <h2 class="start">ログインが完了しました</h2>
+    </form>
+    <footer>Copyright © 2024 CheerParK</footer>
+</body>
+</html>
